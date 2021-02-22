4ff8241195bd31a17ccbb28d597bc797006c4636
API991Shardul <78960010+API991Shardul@users.noreply.github.com>
2021-02-22 16:18:57 +0530

    Another

--- Example.java
+++ Example.java
@@ -2,6 +2,7 @@ class Example {
   
     public static void main(String[] args) 
     {  
-        System.out.println("New Changes by Shardul"); 
+        System.out.println("New Changes by Shardul");
+       System.out.prin("Another change"); 
     } 
 } 
